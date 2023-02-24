456f0cc5f70464b8c7d47c78f5d3f4bc851c59f1
Michael Gibbons <michael.gibbons92407@gmail.com>
2023-02-24 16:30:51 -0700

    making app extension name dynamic

--- extension/src/main.jsx
+++ extension/src/main.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const TARGET_ID = 'OSB-theme-app-extension-customer-portal'
+import toml from 'toml'
+
+const config = toml.parse(fs.readFileSync(resolve(__dirname, 'src', 'shopify.theme.extension.toml'), 'utf8'))
+const EXTENSION_NAME = config.name
+
+const TARGET_ID = `OSB-theme-app-extension-${EXTENSION_NAME}`
 const TARGET = document.getElementById(TARGET_ID)
 
 ReactDOM.createRoot(TARGET).render(
